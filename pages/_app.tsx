39600b968066b4cc744aee61881c1d71ecadb1c6
GageVanK <87737875+GageVanK@users.noreply.github.com>
2024-06-08 21:41:21 +0000

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,7 @@ import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import {
-  AppShell,
-  MantineProvider,
-  Button,
-  ActionIcon,
-  Group,
-  Space,
-  Container,
-  Tooltip,
-} from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+import { MantineProvider } from '@mantine/core';
 import { configure } from 'deso-protocol';
 import { DeSoIdentityProvider } from 'react-deso-protocol';
 import { LivepeerConfig, createReactClient, studioProvider } from '@livepeer/react';
@@ -20,7 +10,7 @@ import { Notifications } from '@mantine/notifications';
 import { MantineAppShell } from '@/components/MantineAppShell/MantineAppShell';
 import { theme } from '../theme';
 import '@mantine/notifications/styles.css';
-import "../global.css"
+import '../global.css';
 
 configure({
   spendingLimitOptions: {
@@ -83,7 +73,13 @@ export default function App({ Component, pageProps }: AppProps) {
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
           />
-          <link rel="shortcut icon" href="/favicon.svg" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <DeSoIdentityProvider>
           <MantineAppShell>
